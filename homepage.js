51c8df26fb8e5b206a66def719db09e3ed610960
deborah <deborahnwankwo1998@gmail.com>
2024-02-14 14:03:17 +0000

    removing comments

--- homepage.js
+++ homepage.js
@@ -1,24 +1,13 @@
 
 let opacity = 0;
 let intervalID = 0;
-
-  const tag = document.getElementById("test");
-  tag.addEventListener('onload', fadeIn);
+const tag = document.getElementById("test");
 
   function fadeIn(){
     intervalID = setInterval(show, 200);
   }
-  // // style property returns the values of an elemen'ts style attribute
-  // const h1_tag = document.querySelector('#test')
-  // h1_tag.addEventListener('click', function(){
-  //   h1_tag.style.color = 'red';
-  // })
-  // // setTimeout(scrollLeft, 25);
-
-  // // scrollLeft();
 
   function show(){
-    // opacity = Number(window.getComputedStyle(tag).getPropertyValue("opacity"));
     console.log(opacity);
     if (opacity < 1) {
       opacity = opacity + 0.1;
